1b2b4e6e7cf562e8478c002915bc6376e4e6cadb
Kirin Rastogi <dipshit.dev@gmail.com>
2020-05-07 12:22:13 -0400

    implementation & err in tests

--- meals_test.go
+++ meals_test.go
@@ -8,20 +8,22 @@ func TestMealify(t *testing.T) {
 	var cookbook = []struct {
 		in       string
 		expected string
+		err      error
 	}{
-		{"🍞🍅🧀", "🍕"},
-		{"🍞🧀🍅", "🍕"},
-		{"🧀🍞🍅", "🍕"},
-		{"🧀🍅🍞", "🍕"},
-		{"🍅🍞🧀", "🍕"},
-		{"🍅🧀🍞", "🍕"},
-		{"💧🍈", "🍉"},
-		{"🍈💧", "🍉"},
+		{"🍞🍅🧀", "🍕", nil},
+		{"🍞🧀🍅", "🍕", nil},
+		{"🧀🍞🍅", "🍕", nil},
+		{"🧀🍅🍞", "🍕", nil},
+		{"🍅🍞🧀", "🍕", nil},
+		{"🍅🧀🍞", "🍕", nil},
+		{"💧🍈", "🍉", nil},
+		{"🍈💧", "🍉", nil},
+		{"🍈🍳💧", "🍉", ErrNotFound},
 	}
 
 	for _, tt := range cookbook {
 		t.Run(tt.in, func(t *testing.T) {
-			if out, _ := Mealify(tt.in); out != tt.expected {
+			if out, err := Mealify(tt.in); out != tt.expected && err != tt.err {
 				t.Fatalf("expected input %s to make %s. Got \"%s\" instead\n", tt.in, tt.expected, out)
 			}
 		})
